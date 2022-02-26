8fc7a6cbe1b4439bd2673a2192c92252c198901c
AqeeAqee <aqee@msn.com>
2022-02-27 02:14:03 +0800

    auto generate bg from default knots pattern

--- main.ts
+++ main.ts
@@ -1,79 +1,100 @@
-/**
- * game.onPaint(()=>{
- */
 // })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.y += -10
-    mySprite.x += -10 * lr
+    sprCursor.y += -10
+    sprCursor.x += -10 * lr
     lr = lr * -1
     scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + -10)
 })
 function genCursor () {
-    mySprite = sprites.create(image.create(screen.width * 2, screen.height * 2), SpriteKind.Player)
-    mySprite.image.drawLine(0, 113, 319, 113, 5)
-    mySprite.image.drawLine(0, 125, 319, 125, 5)
-    mySprite.image.drawLine(153, 0, 153, 239, 5)
-    mySprite.image.drawLine(165, 0, 165, 239, 5)
+    sprCursor = sprites.create(image.create(screen.width * 2, screen.height * 2), SpriteKind.Player)
+    sprCursor.image.drawLine(0, 113, 319, 113, 5)
+    sprCursor.image.drawLine(0, 125, 319, 125, 5)
+    sprCursor.image.drawLine(153, 0, 153, 239, 5)
+    sprCursor.image.drawLine(165, 0, 165, 239, 5)
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    spriteKnotsData.setFlag(SpriteFlag.Invisible, false)
+	
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.x += -20
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.x += 20
-})
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    spriteKnotsData.setFlag(SpriteFlag.Invisible, true)
-})
-function genSpriteBG (countThread: number, countRows: number) {
+// game.onPaint(()=>{
+function genData_DefaultPattern () {
     iKnots = countThread - 1
-    iRows = countRows
-    imgTreadData = image.create(iKnots, iRows)
-    imgKnotsData = image.create(countThread, iRows + 1)
-    iInterval = img1.width - 1
-    bg = image.create(iInterval * iKnots + 1, iInterval * iRows + 1)
-    for (let y = 0; y <= iRows - 1; y++) {
+    interval = img1.width - 1
+    bg = image.create(interval * iKnots + 1, interval * countRows + 1)
+    for (let x = 0; x <= countThread - 1; x++) {
+        bg.setPixel(x * interval, 0, x%2==0?5:2)
+    }
+    // if(y==2)break
+    for (let y = 0; y <= countRows - 1; y++) {
         knotType = y % 4 + 1
-        knotType = 0
-        for (let x = y % 2; x <= iKnots - 1; x++) {
-            imgKnotsData.setPixel(x, y, knotType)
-            imgKnotsData.setPixel(x, y, knotType)
-            imgTemp = getImageFromLocAndType(x, y, knotType)
-            // imgTemp = img1
-            bg.drawTransparentImage(imgTemp, x* (iInterval) , (y ) * (iInterval))
-            x++
+        for (let x = 0; x <= iKnots - 1; x++) {
+            if (x == 0 && y % 2 == 1) {
+                continue;
+            }
+            setKnot(x, y, knotType, false)
+            x += 1
         }
     }
-    // tiles.setCurrentTilemap(tilemap`level2`)
-    spriteBG = sprites.create(bg, SpriteKind.Projectile)
-    sptThreadData = sprites.create(imgTreadData, SpriteKind.Projectile)
-    spriteKnotsData = sprites.create(imgKnotsData, SpriteKind.Projectile)
 }
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprCursor.x += -20
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprCursor.x += 20
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+	
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.y += 10
-    mySprite.x += -10 * lr
+    sprCursor.y += 10
+    sprCursor.x += -10 * lr
     lr = lr * -1
     scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + 10)
 })
-function getImageFromLocAndType (x: number, y: number, _type: number) {
-    return imgList[_type]
+// if (knotType == 0) {
+// if (x == 0||x==countThread-1)
+// return //todo, image at side end
+// else if (bg.getPixel(x * interval - 1, y * interval) != 0 || bg.getPixel(x * interval + 1, y * interval)!=0)
+// return
+// }
+function setKnot (x: number, y: number, knotType: number, autoUpdate: boolean) {
+    // knotType = bg.getPixel(x * iInterval, y * iInterval)
+    imgTemp = imgList[knotType].clone()
+    cNew1 = bg.getPixel(x * interval, y * interval)
+    cNew2 = bg.getPixel((x + 1) * interval, y * interval)
+    c1 = imgTemp.getPixel(0, 0)
+    c2 = imgTemp.getPixel(interval, 0)
+    if (cNew1 == c2) {
+        imgTemp.replace(c2, 15)
+        c2 = 15
+    }
+    imgTemp.replace(c1, cNew1)
+    imgTemp.replace(c2, cNew2)
+    bg.drawTransparentImage(imgTemp, x * interval, y * interval)
+if (x == 0) {
+        // update first thread at next line
+        bg.setPixel(x * interval, (y + 2) * interval, bg.getPixel(x * interval, (y + 1) * interval))
+    }
+    if (x == countThread - 2) {
+        // update last thread at next line
+        bg.setPixel((x + 1) * interval, (y + 2) * interval, bg.getPixel((x + 1) * interval, (y + 1) * interval))
+    }
 }
-let sptThreadData: Sprite = null
-let imgTreadData: Image = null
-let iRows = 0
-let mySprite: Sprite = null
-let spriteKnotsData: Sprite = null
-let spriteBG: Sprite = null
-let lr = 0
-let imgList: Image[] = []
-let bg: Image = null
-let iInterval = 0
-let imgKnotsData = null
-let iKnots = 0
-let imgTemp: Image = null
+let c2 = 0
+let c1 = 0
+let cNew2 = 0
+let cNew1 = 0
 let knotType = 0
+let iKnots = 0
+let sprCursor: Sprite = null
+let lr = 0
+let countRows = 0
+let countThread = 0
+let imgList: Image[] = []
+let imgTemp: Image = null
+let interval = 0
+let bg: Image = null
+let iRows = 0
+let threadEndHeight = 4
 let img0 = img`
     5 . . . . . . . . . 2 
     . 5 . . . . . . . 2 . 
@@ -124,8 +145,16 @@ img2,
 img3,
 img4
 ]
-genSpriteBG(16, 8)
+// for(let i=0;i<imgList.length;i++){
+// imgList[i].replace(2, 22)
+// imgList[i].replace(5, 25)
+// }
+interval = img1.width - 1
+countThread = 8
+countRows = 12
 genCursor()
+let locCursor = new tiles.Location(3,3, null)
+genData_DefaultPattern()
+let spriteBG = sprites.create(bg, SpriteKind.Projectile)
 lr = 1
-spriteBG.setPosition(80, 45)
-spriteKnotsData.setPosition(72, -20)
+spriteBG.setPosition(80, 85)
