5387aacabb0a6cc04e596fe9abbc67e337c4c080
AqeeAqee <aqee@msn.com>
2022-02-27 02:51:46 +0800

    change knot type by button A

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
 // })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    sprCursor.y += -10
-    sprCursor.x += -10 * lr
-    lr = lr * -1
-    scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + -10)
-})
 function genCursor () {
     sprCursor = sprites.create(image.create(screen.width * 2, screen.height * 2), SpriteKind.Player)
     sprCursor.image.drawLine(0, 113, 319, 113, 5)
@@ -12,9 +6,6 @@ function genCursor () {
     sprCursor.image.drawLine(153, 0, 153, 239, 5)
     sprCursor.image.drawLine(165, 0, 165, 239, 5)
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
 // game.onPaint(()=>{
 function genData_DefaultPattern () {
     iKnots = countThread - 1
@@ -37,16 +28,25 @@ function genData_DefaultPattern () {
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     sprCursor.x += -20
+    cursorX+=-2
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     sprCursor.x += 20
+    cursorX += 2
 })
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-	
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprCursor.y += -10
+    sprCursor.x += -10 * lr
+    cursorY += -1
+    cursorX += -1 * lr
+    lr = lr * -1
+    scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + -10)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     sprCursor.y += 10
     sprCursor.x += -10 * lr
+    cursorY += 1
+    cursorX += -1*lr
     lr = lr * -1
     scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + 10)
 })
@@ -56,6 +56,32 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 // else if (bg.getPixel(x * interval - 1, y * interval) != 0 || bg.getPixel(x * interval + 1, y * interval)!=0)
 // return
 // }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    // game.splash(getKnot(cursorX, cursorY))
+    let knotType = getKnotType(cursorX, cursorY)
+    knotType+=1
+    if(knotType==5)
+        knotType=1
+    setKnot(cursorX, cursorY,knotType, true)
+})
+
+function getKnotType(x: number, y: number): number {
+    if (bg.getPixel(x * interval + 5, y * interval + 5) == 1){
+        if (bg.getPixel(x * interval + 6, y * interval + 4) == 1)
+            return 1
+        else if (bg.getPixel(x * interval + 4, y * interval + 4) == 1)
+            return 3
+        else
+            return -1
+    }else{
+        if (bg.getPixel(x * interval + 4, y * interval + 4) == 1)
+            return 2
+        else if (bg.getPixel(x * interval + 6, y * interval + 4) == 1)
+            return 4
+        else
+            return -1
+    }
+}
 function setKnot (x: number, y: number, knotType: number, autoUpdate: boolean) {
     // knotType = bg.getPixel(x * iInterval, y * iInterval)
     imgTemp = imgList[knotType].clone()
@@ -70,13 +96,23 @@ function setKnot (x: number, y: number, knotType: number, autoUpdate: boolean) {
     imgTemp.replace(c1, cNew1)
     imgTemp.replace(c2, cNew2)
     bg.drawTransparentImage(imgTemp, x * interval, y * interval)
-if (x == 0) {
-        // update first thread at next line
+
+    // update first thread at next line
+    if (x == 0) {
         bg.setPixel(x * interval, (y + 2) * interval, bg.getPixel(x * interval, (y + 1) * interval))
-    }
+        }
+    // update last thread at next line
     if (x == countThread - 2) {
-        // update last thread at next line
         bg.setPixel((x + 1) * interval, (y + 2) * interval, bg.getPixel((x + 1) * interval, (y + 1) * interval))
+    }
+    info.changeScoreBy(1)
+    // basic.pause(100)
+
+    if(autoUpdate&&y<countRows-1){
+        for(let i=Math.max(x-1,0); i<Math.min(x+2, countThread-1);i++){
+            if(getKnotType(i,y+1)>0)
+                setKnot(i, y+1, getKnotType(i, y + 1),true)
+        }
     }
 }
 let c2 = 0
@@ -96,44 +132,44 @@ let bg: Image = null
 let iRows = 0
 let threadEndHeight = 4
 let img0 = img`
-    5 . . . . . . . . . 2 
-    . 5 . . . . . . . 2 . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . 5 . . . . . . . 2 . 
-    5 . . . . . . . . . 2 
-    `
+    5 . . . . . . . . . 2
+    . 5 . . . . . . . 2 .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . 5 . . . . . . . 2 .
+    5 . . . . . . . . . 2
+`
 let img1 = img`
-    5 . . 2 2 2 2 2 . . 2 
-    . 5 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 . 
-    2 2 2 2 2 2 2 1 2 2 2 
-    2 2 2 2 2 2 1 2 2 2 2 
-    2 2 1 2 2 1 2 2 2 2 2 
-    2 2 1 2 1 2 2 2 2 2 2 
-    2 2 1 1 2 2 2 2 2 2 2 
-    2 2 1 1 1 1 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 5 . 
-    2 . . 2 2 2 2 2 . . 5 
-    `
+    5 . . 2 2 2 2 2 . . 2
+    . 5 2 2 2 2 2 2 2 2 .
+    . 2 2 2 2 2 2 2 2 2 .
+    2 2 2 2 2 2 2 1 2 2 2
+    2 2 2 2 2 2 1 2 2 2 2
+    2 2 1 2 2 1 2 2 2 2 2
+    2 2 1 2 1 2 2 2 2 2 2
+    2 2 1 1 2 2 2 2 2 2 2
+    2 2 1 1 1 1 2 2 2 2 2
+    . 2 2 2 2 2 2 2 2 5 .
+    2 . . 2 2 2 2 2 . . 5
+`
 let img2 = img`
-    5 . . 2 2 2 2 2 . . 2 
-    . 5 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 1 2 2 2 . 
-    2 2 2 2 2 1 2 2 2 2 2 
-    2 2 2 2 1 2 2 2 2 2 2 
-    2 2 2 1 2 2 2 2 2 2 2 
-    2 2 2 2 1 2 2 1 2 2 2 
-    2 2 2 2 2 1 2 1 2 2 2 
-    . 2 2 2 2 2 1 1 2 2 . 
-    . 5 2 2 1 1 1 1 2 2 . 
-    5 . . 2 2 2 2 2 . . 2 
-    `
+    5 . . 2 2 2 2 2 . . 2
+    . 5 2 2 2 2 2 2 2 2 .
+    . 2 2 2 2 2 1 2 2 2 .
+    2 2 2 2 2 1 2 2 2 2 2
+    2 2 2 2 1 2 2 2 2 2 2
+    2 2 2 1 2 2 2 2 2 2 2
+    2 2 2 2 1 2 2 1 2 2 2
+    2 2 2 2 2 1 2 1 2 2 2
+    . 2 2 2 2 2 1 1 2 2 .
+    . 5 2 2 1 1 1 1 2 2 .
+    5 . . 2 2 2 2 2 . . 2
+`
 let img3 = img1.clone()
 img3.flipX()
 let img4 = img2.clone()
@@ -153,7 +189,8 @@ interval = img1.width - 1
 countThread = 8
 countRows = 12
 genCursor()
-let locCursor = new tiles.Location(3,3, null)
+let cursorX =3
+let cursorY = 3
 genData_DefaultPattern()
 let spriteBG = sprites.create(bg, SpriteKind.Projectile)
 lr = 1
