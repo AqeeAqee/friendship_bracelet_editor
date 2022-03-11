a8bdc6b0dd8c1c204fdc437865c9dee31f1af4d8
AqeeAqee <aqee@msn.com>
2022-03-11 10:09:02 +0800

    enlarged knot image

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 function genThread () {
     imgThreads = image.create(imgKnots.width, threadEndHeight)
     for (let l = 0; l <= countThread - 1; l++) {
-        imgThreads.drawLine(l * interval, 0, l * interval, threadEndHeight, imgKnots.getPixel(l * interval, 0))
+        imgThreads.drawLine(l * interval, 0, l * interval, threadEndHeight, imgKnots.getPixel(l * interval, 0 + knotOffY))
     }
     return imgThreads
 }
@@ -29,12 +29,6 @@ function genCursor () {
     sprCursor = sprites.create(image.create(interval * 2, interval * 2), SpriteKind.Player)
     sprCursor.image.fill(9)
 }
-// if (knotType == 0) {
-// if (x == 0||x==countThread-1)
-// return //todo, image at side end
-// else if (bg.getPixel(x * interval - 1, y * interval) != 0 || bg.getPixel(x * interval + 1, y * interval)!=0)
-// return
-// }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     switchKnotType_Color()
 })
@@ -46,7 +40,6 @@ function setKnotAndUpdate (x: number, y: number, knotType: number) {
             if (imgToUpdateKnots.getPixel(i, j) > 0)
                 setKnot(i, j, getKnotType(i, j), imgToUpdateKnots)
         }
-        imgToUpdateKnots = imgToUpdateKnots
     }
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -58,8 +51,8 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 function showTitle () {
     color.setPalette(color.Black)
     scene.setBackgroundImage(image.create(screen.width,screen.height))
-    scene.backgroundImage().drawImage(imgTitle,10,10)
-    scene.backgroundImage().print("by Aqee", 106,106)
+    scene.backgroundImage().drawImage(imgTitle,10,30)
+    // scene.backgroundImage().print("by Aqee", 58,106)
     color.startFade(color.Black, color.originalPalette, 2000)
     color.pauseUntilFadeDone()
     pause(500)
@@ -86,6 +79,7 @@ function switchKnotType_Next () {
 }
 function switchKnotType_Color () {
     knotType = getKnotType(cursorX, cursorY)
+    // game.splash(knotType)
     switchList = [
     0,
     3,
@@ -103,18 +97,21 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 function getKnotType (x: number, y: number) {
-    if (imgKnots.getPixel(x * interval + 5, y * interval + 5) == 1) {
-        if (imgKnots.getPixel(x * interval + 6, y * interval + 4) == 1) {
+    // imgKnots.setPixel(x * interval + 5, y * interval + 5 + knotOffY, 4)
+    // imgKnots.setPixel(x * interval + 4, y * interval + 4 + knotOffY, 4)
+    // imgKnots.setPixel(x * interval + 6, y * interval + 4 + knotOffY, 4)
+    if (imgKnots.getPixel(x * interval + 5, y * interval + 5 + knotOffY) == 1) {
+        if (imgKnots.getPixel(x * interval + 6, y * interval + 4 + knotOffY) == 1) {
             return 1
-        } else if (imgKnots.getPixel(x * interval + 4, y * interval + 4) == 1) {
+        } else if (imgKnots.getPixel(x * interval + 4, y * interval + 4 + knotOffY) == 1) {
             return 3
         } else {
             return -1
         }
     } else {
-        if (imgKnots.getPixel(x * interval + 4, y * interval + 4) == 1) {
+        if (imgKnots.getPixel(x * interval + 4, y * interval + 4 + knotOffY) == 1) {
             return 2
-        } else if (imgKnots.getPixel(x * interval + 6, y * interval + 4) == 1) {
+        } else if (imgKnots.getPixel(x * interval + 6, y * interval + 4 + knotOffY) == 1) {
             return 4
         } else {
             return -1
@@ -142,13 +139,7 @@ function updatePreview (x: number, y: number, c: number) {
 }
 function switchKnotType_Thread () {
     knotType2 = getKnotType(cursorX, cursorY)
-    switchList2 = [
-    0,
-    2,
-    1,
-    4,
-    3
-    ]
+    switchList2 = [0,2,1,4,3]
     knotType2 = switchList2[knotType2]
     // info.setScore(0)
     setKnotAndUpdate(cursorX, cursorY, knotType2)
@@ -156,10 +147,10 @@ function switchKnotType_Thread () {
 function setKnot (x: number, y: number, knotType: number, imgToUpdateKnots: Image) {
     // knotType = bg.getPixel(x * iInterval, y * iInterval)
     imgTemp = imgList[knotType].clone()
-    cNew1 = imgKnots.getPixel(x * interval, y * interval)
-    cNew2 = imgKnots.getPixel((x + 1) * interval, y * interval)
-    c1 = imgTemp.getPixel(0, 0)
-    c2 = imgTemp.getPixel(interval, 0)
+    cNew1 = imgKnots.getPixel(x * interval, y * interval+knotOffY)
+    cNew2 = imgKnots.getPixel((x + 1) * interval, y * interval + knotOffY)
+    c1 = imgTemp.getPixel(0, 2)
+    c2 = imgTemp.getPixel(interval, 2)
     if (cNew1 == c2) {
         imgTemp.replace(c2, 15)
         c2 = 15
@@ -167,20 +158,20 @@ function setKnot (x: number, y: number, knotType: number, imgToUpdateKnots: Imag
     imgTemp.replace(c1, cNew1)
     imgTemp.replace(c2, cNew2)
     imgKnots.drawTransparentImage(imgTemp, x * interval, y * interval)
-// update first thread at next line
+    // update first thread at next row
     if (x == 0) {
-        imgKnots.drawLine(x * interval, (y + 1) * interval, x * interval, (y + 2) * interval, imgKnots.getPixel(x * interval, (y + 1) * interval))
+        imgKnots.drawLine(x * interval, (y + 1) * interval + knotOffY , x * interval, (y + 2) * interval + knotOffY , imgKnots.getPixel(x * interval, (y + 1) * interval + knotOffY) )
     }
-    // update last thread at next line
+    // update last thread at next row
     if (x == countThread - 2) {
-        imgKnots.drawLine((x + 1) * interval, (y + 1) * interval, (x + 1) * interval, (y + 2) * interval, imgKnots.getPixel((x + 1) * interval, (y + 1) * interval))
+        imgKnots.drawLine((x + 1) * interval, (y + 1) * interval + knotOffY , (x + 1) * interval, (y + 2) * interval + knotOffY , imgKnots.getPixel((x + 1) * interval, (y + 1) * interval + knotOffY) )
     }
     // todo, update outer threads when last row
     // info.changeScoreBy(1)
     updatePreview(x, y, [0,cNew2,cNew2,cNew1,cNew1].get(knotType))
     if (imgToUpdateKnots && y < countRows - 1) {
         basic.pause(10)
-for(let k=Math.max(x-1,0); k<Math.min(x+2, countThread-1);k++){
+    for(let k=Math.max(x-1,0); k<Math.min(x+2, countThread-1);k++){
             if(getKnotType(k,y+1)>0)
                 imgToUpdateKnots.setPixel(k,y+1,1)
                 //setKnot(i, y+1, getKnotType(i, y + 1),true)
@@ -188,15 +179,33 @@ for(let k=Math.max(x-1,0); k<Math.min(x+2, countThread-1);k++){
     }
     return imgToUpdateKnots
 }
+let imgt=img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`
 function genKnots_DefaultPattern () {
     iKnots = countThread - 1
     interval = img1.width - 1
-    imgKnots = image.create(interval * iKnots + 1, interval * countRows + 1)
+    imgKnots = image.create(interval * iKnots + 1, interval * countRows + 1 + knotOffY*2)
     for (let x = 0; x <= countThread - 1; x++) {
-        imgKnots.setPixel(x * interval, 0, x%2==0?7:2)
+        imgKnots.drawLine(x * interval, 0, x * interval, knotOffY, x%2==0?10:3)
     }
     // if(y==2)break
-    for (let y = 0; y <= countRows - 1; y++) {
+    for (let y = 0; y <= countRows -1; y++) {
         knotType4 = y % 4 + 1
         for (let x2 = 0; x2 <= iKnots - 1; x2++) {
             if (x2 == 0 && y % 2 == 1) {
@@ -207,6 +216,8 @@ function genKnots_DefaultPattern () {
         }
     }
 }
+// showTitle()
+let knotOffY = 2
 let knotType4 = 0
 let iKnots = 0
 let c2 = 0
@@ -232,46 +243,53 @@ let countThread = 0
 let countRows = 0
 let cNew1 = 0
 let cNew2 = 0
-showTitle()
 let img0 = img`
-    5 . . . . . . . . . 2 
-    . 5 . . . . . . . 2 . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . . 5 . . . . . 2 . . 
-    . 5 . . . . . . . 2 . 
-    5 . . . . . . . . . 2 
-    `
+    5 . . . . . . . . . 2
+    . 5 . . . . . . . 2 .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . . 5 . . . . . 2 . .
+    . 5 . . . . . . . 2 .
+    5 . . . . . . . . . 2
+`
 let img1 = img`
-    5 . . 2 2 2 2 2 . . 2 
-    . 5 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 . 
-    2 2 2 2 2 2 2 1 2 2 2 
-    2 2 2 2 2 2 1 2 2 2 2 
-    2 2 1 2 2 1 2 2 2 2 2 
-    2 2 1 2 1 2 2 2 2 2 2 
-    2 2 1 1 2 2 2 2 2 2 2 
-    2 2 1 1 1 1 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 5 . 
-    2 . . 2 2 2 2 2 . . 5 
-    `
+    . . . . 2 2 2 . . . .
+    . . . 2 2 2 2 2 . . .
+    5 5 2 2 2 2 2 2 2 2 2
+    5 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 1 2 2 2
+    2 2 2 2 2 2 1 2 2 2 2
+    2 2 1 2 2 1 2 2 2 2 2
+    2 2 1 2 1 2 2 2 2 2 2
+    2 2 1 1 2 2 2 2 2 2 2
+    2 2 1 1 1 1 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 5
+    2 2 2 2 2 2 2 2 2 5 5
+    . . . 2 2 2 2 2 . . .
+    . . . . 2 2 2 . . . .
+`
 let img2 = img`
-    5 . . 2 2 2 2 2 . . 2 
-    . 5 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 1 2 2 2 . 
-    2 2 2 2 2 1 2 2 2 2 2 
-    2 2 2 2 1 2 2 2 2 2 2 
-    2 2 2 1 2 2 2 2 2 2 2 
-    2 2 2 2 1 2 2 1 2 2 2 
-    2 2 2 2 2 1 2 1 2 2 2 
-    . 2 2 2 2 2 1 1 2 2 . 
-    . 5 2 2 1 1 1 1 2 2 . 
-    5 . . 2 2 2 2 2 . . 2 
-    `
+    . . . . 2 2 2 . . . .
+    . . . 2 2 2 2 2 . . .
+    5 5 2 2 2 2 2 2 2 2 2
+    5 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 1 2 2 2 2
+    2 2 2 2 2 1 2 2 2 2 2
+    2 2 2 2 1 2 2 2 2 2 2
+    2 2 2 1 2 2 2 2 2 2 2
+    2 2 2 2 1 2 2 1 2 2 2
+    2 2 2 2 2 1 2 1 2 2 2
+    2 2 2 2 2 2 1 1 2 2 2
+    5 2 2 2 1 1 1 1 2 2 2
+    5 5 2 2 2 2 2 2 2 2 2
+    . . . 2 2 2 2 2 . . .
+    . . . . 2 2 2 . . . .
+`
 let img3 = img1.clone()
 img3.flipX()
 let img4 = img2.clone()
@@ -284,11 +302,11 @@ img3,
 img4
 ]
 interval = img1.width - 1
-countThread = 12
+countThread = 8
 countRows = 16
 cursorLR = 1
 genCursor()
-cursorX = 5
+cursorX = 3
 cursorY = 5
 let sprPreview:Sprite
 genPreview()
